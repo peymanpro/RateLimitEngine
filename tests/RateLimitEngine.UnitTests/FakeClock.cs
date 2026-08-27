e9adedd48430201df2c73b4accd8a2b511ac7144
Your Name <you@company.com>
2026-08-27 06:52:34 +0330

    test: wire unit tests and add fake clock

---
+++ tests/RateLimitEngine.UnitTests/FakeClock.cs
@@ -0,0 +1,30 @@
+using RateLimitEngine.Core.Time;
+
+namespace RateLimitEngine.UnitTests;
+
+internal sealed class FakeClock : IClock
+{
+    private DateTimeOffset _utcNow;
+
+    public FakeClock(DateTimeOffset utcNow)
+    {
+        _utcNow = utcNow;
+    }
+
+    public DateTimeOffset UtcNow => _utcNow;
+
+    public long GetTimestamp() => _utcNow.Ticks;
+
+    public TimeSpan GetElapsedTime(long startingTimestamp) =>
+        TimeSpan.FromTicks(_utcNow.Ticks - startingTimestamp);
+
+    public void Advance(TimeSpan duration)
+    {
+        if (duration < TimeSpan.Zero)
+        {
+            throw new ArgumentOutOfRangeException(nameof(duration));
+        }
+
+        _utcNow = _utcNow.Add(duration);
+    }
+}
